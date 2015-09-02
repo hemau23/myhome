06e3e7bf302b857807ef561179f82bdc117f9712
unknown <hemau23@gmail.com>
2015-09-01 21:50:05 -0500

    brothers data

--- data/bat.bat
+++ data/bat.bat
@@ -3,7 +3,7 @@
 :LOOP
 
 time /t
-start D:\program\apache-jmeter-2.13\bin\jmeter -n -t D:\myhome\scripts\getData.jmx
+ echo "D:\program\apache-jmeter-2.13\bin\jmeter" -n -t "D:\myhome\scripts\getData.jmx"
 
 TIMEOUT /T 900 /NOBREAK
 GOTO LOOP
